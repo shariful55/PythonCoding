5de547b8c1fb67a0add2465d3d670d667e2c682c
Shariful Islam <shariful01944180957@gmail.com>
2021-07-27 11:41:35 +0600

    first face practice
    first face

---
+++ input.py
@@ -0,0 +1,9 @@
+name=input("enter your name here ")
+print(name)
+
+name=input("enter your phone number ")
+print(name)
+
+name=input("enter your address ")
+print(name)
+
---
+++ typecasting.py
@@ -0,0 +1,18 @@
+num1=int(input("enter first num"))
+num2=int(input("enter last num"))
+
+print("Total integer sum",num1+num2)
+
+num1=float(input("enter first float"))
+num2=float(input("enter last float"))
+print("Total float sum", num1+num2)
+
+#2302-910
+
+string=str(input("input string"))
+print("My information is:",string)
+
+string1=input("input like that")
+print("What's going on",string1)
+
+
